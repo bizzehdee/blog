00061f1d123e0b71615bf4d0bc8b8622864530b7
Darren Horrocks <darren.horrocks@airsweb.com>
2023-09-26 10:42:23 +0100

    script fix

--- hugo-new.ps1
+++ hugo-new.ps1
@@ -14,7 +14,7 @@ $result = $result -replace "[^a-z0-9\s]+", ""
 $result = $result -replace "\s+", "-"
 
 $Command = "hugo.exe"
-$Parms = "new content $Result.md"
+$Parms = "new $Result.md"
 
 $Parms = $Parms.Split(" ")
 & "$Command" $Parms
